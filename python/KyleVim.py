d8561476ff11f79f5227c180c493885c4dcd5beb
KyleS22 <25314187+KyleS22@users.noreply.github.com>
2019-08-07 19:36:43 -0600

    Add python structure

---
+++ python/KyleVim.py
@@ -0,0 +1,12 @@
+"""
+File Name: 
+
+Authors: Kyle Seidenthal
+
+Date: 07-08-2019
+
+Description: The base python script for accessing the plugin from vim
+
+"""
+
+print("Hell from python source code!")
